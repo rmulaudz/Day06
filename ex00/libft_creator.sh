ee47582d6423f17dff1550f717746d84b8f6dd1a
Ruddik Mulaudzi <rmulaudz@c4r6s8.wethinkcode.co.za>
2020-06-26 14:35:33 +0200

    Finished

---
+++ ex00/libft_creator.sh
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    libft_creator.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rmulaudz <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/06/26 14:15:31 by rmulaudz          #+#    #+#              #
+#    Updated: 2020/06/26 14:30:49 by rmulaudz         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
